a976e073973170a969b27d99d497bd09201fdddc
Michael Ozoemena <michaelozoemena2@gmail.com>
2019-08-05 15:20:44 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from "~/layouts/Default.vue";
+import VueFullPage from "vue-fullpage.js";
+
+export default function(Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.7/fullpage.css"
+  });
+
+  Vue.use(VueFullPage);
+
+  Vue.component("Layout", DefaultLayout);
+}
